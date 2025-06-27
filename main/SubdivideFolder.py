fd88a1ad4381ec66dedfed8c24697039f52f3741
DLRX <dodo.leroux@outlook.com>
2025-06-27 11:35:01 +0200

    add a subdivide code

---
+++ main/SubdivideFolder.py
@@ -0,0 +1,59 @@
+import os
+import shutil
+from collections import defaultdict
+from tqdm import tqdm
+
+def print_infos(output):
+    folder_stats = []
+    for folder_name in os.listdir(output):
+        folder_path = os.path.join(output, folder_name)
+        if os.path.isdir(folder_path):
+            total_size = 0
+            file_count = 0
+            for dirpath, dirnames, filenames in os.walk(folder_path):
+                for f in filenames:
+                    fp = os.path.join(dirpath, f)
+                    total_size += os.path.getsize(fp)
+                    file_count += 1
+            folder_stats.append((folder_name, total_size, file_count))
+
+    for name, size, count in folder_stats:
+        print(f"{name}: {size} octets, {count} fichiers")
+
+
+def subdivide_folder_by_size(input_dir, output_dir, n_subdivided):
+    """
+    Subdivide a folder into n_subdivided subfolders with balanced total file size.
+    """
+    files = [(f, os.path.getsize(os.path.join(input_dir, f))) for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+    files_sorted = sorted(files, key=lambda x: x[1], reverse=True)
+
+    # Create subfolders and track their total size
+    subfolders = []
+    subfolder_sizes = []
+    for i in range(n_subdivided):
+        subfolder_name = f"subfolder_{i+1}"
+        subfolder_path = os.path.join(output_dir, subfolder_name)
+        os.makedirs(subfolder_path, exist_ok=True)
+        subfolders.append(subfolder_path)
+        subfolder_sizes.append(0)
+
+    # Greedy assignment: always add next largest file to the subfolder with the smallest total size
+    for fname, fsize in tqdm(files_sorted):
+        idx = subfolder_sizes.index(min(subfolder_sizes))
+        dst_folder = subfolders[idx]
+        src_path = os.path.join(input_dir, fname)
+        dst_path = os.path.join(dst_folder, fname)
+        shutil.copy2(src_path, dst_path)
+        subfolder_sizes[idx] += fsize
+
+    print(print_infos(output_dir))
+
+
+
+
+# Utilisation
+subdivide_folder_by_size(input, output, n_sub)
+
+
+
--- main/score_aln.py
+++ main/score_aln.py
@@ -11,22 +11,26 @@ def readBlosum(nameFile):
 	Output1 Blosum: a dictionary containing BLOSUM matrix, key is a pair of amino acids (alphabetic order) and value is the substitution value. 
 	REMARK : You should eliminate the last four columns and last four rows of Blosum file
 	"""
-	Blosum={}
+	Blosum = {}
+	fi = open(nameFile,"r")
+	ligne = fi.readline()
 
-	fi=open(nameFile,"r")
-	ligne=fi.readline()
-	while ligne[0]=="#":
-		ligne=fi.readline()
-	ligneAA=ligne.split()
-	ligneAA=ligneAA[0:len(ligneAA)] #-1] I commented that because some pair-wises were not in dict (show in next python cell)
-	ligne=fi.readline()
-	while len(ligne)>0:
-		ligne=ligne.split()
+	while ligne[0] == "#":
+		ligne = fi.readline()
+
+	ligneAA = ligne.split()
+	ligneAA = ligneAA[0:len(ligneAA)]
+	ligne = fi.readline() 
+
+	while len(ligne) > 0:
+		ligne = ligne.split()
+
 		for j in range(0,len(ligneAA)-1,1):
-			Blosum[ligne[0]+ligneAA[j]] = ligne[j+1]
+			Blosum[ligne[0]+ligneAA[j]]= ligne[j+1]
 		ligne=fi.readline()
 	fi.close()
 	return Blosum
+
 
 #==================================================================================================================
 
@@ -37,7 +41,6 @@ def calculeScore(AA,AA2,blosum):
 	except : 
 		score = blosum[AA2+AA]
 	return int(score)
-
 #==================================================================================================================
 
 def extraireAllFastaMulti(fileName):  
@@ -53,7 +56,6 @@ def extraireAllFastaMulti(fileName):
 	se=""
 	IdSeq=[]
 	CurrentLine  = f.readline()
-	
 	while CurrentLine != "":
 		if CurrentLine[0] != ">"  :
 			se=se+CurrentLine.rstrip("\n")
@@ -79,25 +81,22 @@ def SP_score(SeqsM, blosum, gap=-5):
 	input3 gap: gap penality
 	output1 score: MSA score
 	"""
-	L = len(SeqsM[0]) # aligned sequence sizes (columns)
-	n = len(SeqsM) # number of sequences in clusters (lines)
-	s = 0 # initiate the score
-	
-	for col in tqdm(range(L)):
-		for seq1 in range(n):
-			for seq2 in range(seq1+1, n):
+	Long = len(SeqsM[0])
+	s = 0
+	n = len(SeqsM)
+
+	for p in range(Long):
+		for seq1 in range(len(SeqsM)) :
+			for seq2 in range(seq1+1,len(SeqsM)):
 				seq1p = SeqsM[seq1]
 				seq2p = SeqsM[seq2]
-
-				if seq1p[col] != '-' and seq2p[col] != '-':
-					s1 = seq1p[col]
-					s2 = seq2p[col]
+				if p < len(seq1p) and p < len(seq2p) and seq1p[p] != '-' and seq2p[p] != '-':
+					s1 = seq1p[p]
+					s2 = seq2p[p]
 					s += calculeScore(s1, s2, blosum)
-				else:
+				else :
 					s += gap
-						
-	return s/(L*n)
-
+	return s, n, Long
 
 #==================================================================================================================
 #                                               MAIN
@@ -109,16 +108,29 @@ def main():
     parser.add_option("-i", "--input_file", dest="input_file", help="the folder with the alignement files")
     parser.add_option("-b", "--blosum_file", dest="blosum_file", help="the BLOSUM matrix file to use")
     parser.add_option("-o", "--output_file", dest="output_file", help="the output CSV file to write the scores")
+    parser.add_option('-s', '--smallest', dest='smallest', type='int', default=1, help='Number of smallest files to process (by size). If 0, process all files.')
          
     (options, args) = parser.parse_args()
     folder_aln = options.input_file
     blosum = options.blosum_file
     output_file = options.output_file
+    born_smallest = options.smallest
 
     blosum = readBlosum(blosum)
 	
     if folder_aln:
-		
+
+  
+        files_with_sizes = [(f, os.path.getsize(os.path.join(folder_aln, f))) for f in os.listdir(folder_aln) if os.path.isfile(os.path.join(folder_aln, f))]
+        files_with_sizes_sorted = sorted(files_with_sizes, key=lambda x: x[1])
+        set_50_1st = files_with_sizes_sorted[:born_smallest]
+
+        interet = [f for f, s in set_50_1st]
+
+
+
+
+
         list_f = [f for f in os.listdir(folder_aln) if os.path.isfile(os.path.join(folder_aln, f))]
 		
         len_file = len(list_f)
@@ -133,7 +145,7 @@ def main():
             IdSeq, Seqs = extraireAllFastaMulti(fileName)
             score = SP_score(Seqs, blosum, gap=-5)
 			
-            dict_scores[CK] = score
+            dict_scores[CK] = scorehro
     else:
         print("error with -i : please provide a folder with alignements")
 	
