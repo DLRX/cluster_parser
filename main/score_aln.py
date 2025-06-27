c873b133429e13ece16f9ef6d4f0d0c18143391d
DLRX <dodo.leroux@outlook.com>
2025-06-27 14:15:13 +0200

    final version

--- main/score_aln.py
+++ main/score_aln.py
@@ -40,6 +40,7 @@ def calculeScore(AA,AA2,blosum):
 	except : 
 		score = blosum[AA2+AA]
 	return int(score)
+
 #==================================================================================================================
 
 def extraireAllFastaMulti(fileName):  
@@ -71,7 +72,6 @@ def extraireAllFastaMulti(fileName):
 
 #==================================================================================================================
 
-
 def SP_score(SeqsM, blosum, gap=-5):
 	"""
 	Compute MSA score based on a substitution matrix and gap penality
@@ -102,52 +102,55 @@ def SP_score(SeqsM, blosum, gap=-5):
 #==================================================================================================================
 
 def main():
-    usage = "python ck_parser.py -i <input_file> -b <blosum_file> -o <output_dir> \n" 
-    parser = OptionParser(usage)
-    parser.add_option("-i", "--input_file", dest="input_file", help="the folder with the alignement files")
-    parser.add_option("-b", "--blosum_file", dest="blosum_file", help="the BLOSUM matrix file to use")
-    parser.add_option("-o", "--output_file", dest="output_file", help="the output CSV file to write the scores")
-    parser.add_option('-s', '--smallest', dest='smallest', type='int', default=1, help='Number of smallest files to process (by size). If 0, process all files.')
-         
-    (options, args) = parser.parse_args()
-    folder_aln = options.input_file
-    blosum = options.blosum_file
-    output_file = options.output_file
-    born_smallest = options.smallest
+	usage = "python score_aln.py -i <input_file> -b <blosum_file> -o <output_dir> -s <smallest>\n" 
+	parser = OptionParser(usage)
+	parser.add_option("-i", "--input_file", dest="input_file", help="the folder with the alignement files")
+	parser.add_option("-b", "--blosum_file", dest="blosum_file", help="the BLOSUM matrix file to use")
+	parser.add_option("-o", "--output_file", dest="output_file", help="the output CSV file to write the scores")
+	parser.add_option('-s', '--smallest', dest='smallest', type='int', default=1, help='Number of smallest files to process (by size). If 0, process all files.')
+		 
+	(options, args) = parser.parse_args()
+	folder_aln = options.input_file
+	blosum1 = options.blosum_file
+	output_file = options.output_file
+	born_smallest = options.smallest
 
-    blosum = readBlosum(blosum)
+	print(f"folder_aln: {folder_aln}")
+	print(f"blosum1: {blosum1}")
+	print(f"output_file: {output_file}")
+	print(f"born_smallest: {born_smallest}")
+
+	blosum = readBlosum(blosum1)
 	
-    if folder_aln:
+	if folder_aln:
 
-  
-        files_with_sizes = [(f, os.path.getsize(os.path.join(folder_aln, f))) for f in os.listdir(folder_aln) if os.path.isfile(os.path.join(folder_aln, f))]
-        files_with_sizes_sorted = sorted(files_with_sizes, key=lambda x: x[1])
-        set_50_1st = files_with_sizes_sorted[:born_smallest]
+		files_with_sizes = [(f, os.path.getsize(os.path.join(folder_aln, f))) for f in os.listdir(folder_aln) if os.path.isfile(os.path.join(folder_aln, f))]
+		files_with_sizes_sorted = sorted(files_with_sizes, key=lambda x: x[1])
+		set_50_1st = files_with_sizes_sorted[:born_smallest]
 
-        interet = [f for f, s in set_50_1st]
+		interet = [f for f, s in set_50_1st]
 
-        list_f = [f for f in os.listdir(folder_aln) if os.path.isfile(os.path.join(folder_aln, f))]
+		list_f = [f for f in os.listdir(folder_aln) if os.path.isfile(os.path.join(folder_aln, f))]
 		
-        len_file = len(list_f)
-        dict_scores = {}
-        i = 1
+		#len_file = len(list_f)
+		dict_scores = {}
+		i = 1
+		
+		for CK in tqdm(interet):
+			i += 1
+			fileName = os.path.join(folder_aln, CK)
 
-        for CK in list_f:
-            print("Processing.", CK, "file ", i,"/",len_file )
-            i += 1
-            fileName = os.path.join(folder_aln, CK)
+			IdSeq, Seqs = extraireAllFastaMulti(fileName)
+			s, n, Long = SP_score(Seqs, blosum, gap=-5)
+			dict_scores[CK] = [s, n, Long]
 
-            IdSeq, Seqs = extraireAllFastaMulti(fileName)
-            score = SP_score(Seqs, blosum, gap=-5)
-			
-            dict_scores[CK] = scorehro
-    else:
-        print("error with -i : please provide a folder with alignements")
-	
-    # Save dict_scores to CSV
-    df = pd.DataFrame(list(dict_scores.items()), columns=['filename', 'score'])
-    df.to_csv(output_file, index=False)
+		df_scores = pd.DataFrame.from_dict(dict_scores, orient='index', columns=['score', 'n', 'Long'])
+		df_scores['score/(L*n)'] = df_scores['score'] / (df_scores['n'] * df_scores['Long'])
+		df_scores.index.name = 'cluster'
 
+		df_scores.reset_index(inplace=True)
+		df_scores.to_csv(os.path.join(output_file, "score_clusters.csv"), index=False)
+		print(f"Le fichier est sauvegardé à : {os.path.abspath(output_file)}")
 
 
 #==================================================================================================================
